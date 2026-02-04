1105b7156d49d17fbea581f7b510a484db91eb5c
Orhun Kenger <orhunkenger1929@gmail.com>
2026-02-04 20:10:32 +0300

    Prod URL yerel IP olarak ayarlandi

--- lib/core/config/env_config.dart
+++ lib/core/config/env_config.dart
@@ -3,9 +3,12 @@ import 'package:flutter/foundation.dart';
 class EnvConfig {
   static const String _devBaseUrl = 'http://192.168.1.25:8000';
   static const String _devWebBaseUrl = 'http://192.168.1.25:8000';
-  static const String _prodBaseUrl = 'https://api.kibrisoto.com';
+  
+  // DÜZELTİLDİ: Release modda da senin bilgisayara bakacak
+  static const String _prodBaseUrl = 'http://192.168.1.25:8000';
 
   static String get baseUrl {
+    // Release mod (bizim github build) artık prodBaseUrl'i, yani senin IP'yi kullanacak
     if (kReleaseMode) return _prodBaseUrl;
     return kIsWeb ? _devWebBaseUrl : _devBaseUrl;
   }
